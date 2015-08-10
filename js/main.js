199318e7b7709ff8145c4402b8fce62824922d50
brandon932 <bthornton_932@yahoo.com>
2015-08-09 23:44:57 -0600

    all loops working

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 // add scripts
 var students = ['joe','carl','rachel','derek','lily'];
 $(document).on('ready', function() {
-  console.log('sanity check!');
+  // for (var i = 0; i < students.length; i++) {
+  //   console.log(students[i]);
+  // }
+  // for (var i = 0; i < 4 ; i++) {
+  //    console.log(students[i]);
+  // }
+  // for (var i = 0; i < students.length; i+=2) {
+  //   console.log(students[i]);
+  // }
+  // for (var i = students.length; i >= 0 ; i--) {
+  //   console.log(students[i]);
+  // }
   for (var i = 0; i < students.length; i++) {
-    console.log(students[i]);
+    if (i === 0 || i === students.length -1){
+      console.log(students[i]);
+    }else{
+      console.log(students[i]);
+      console.log(students[i]);
+    }
   }
+
 });
